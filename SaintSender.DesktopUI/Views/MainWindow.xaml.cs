924226f9b6faba05ffbeb3da3cf078efaf8f2213
daniel-kalocsay <kdani782@gmail.com>
2019-12-02 13:53:59 +0100

    GmailQuickstart moved to Core.Services

--- SaintSender.DesktopUI/Views/MainWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Google.Apis.Gmail.v1;
 using SaintSender.Core.Services;
 
 namespace SaintSender.DesktopUI
