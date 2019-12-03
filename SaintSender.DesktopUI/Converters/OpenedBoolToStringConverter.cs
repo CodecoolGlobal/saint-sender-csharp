241621b95f2a8a5c6c72eb6a4748e49dd128b84f
Bence Kovács <kovacs.ben.p@gmail.com>
2019-12-03 11:15:12 +0100

    added opened bool converter

---
+++ SaintSender.DesktopUI/Converters/OpenedBoolToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SaintSender.DesktopUI.Converters
+{
+    public class OpenedBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool opened = (bool)value;
+            if (!opened)
+            {
+                return "Not Opened";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
