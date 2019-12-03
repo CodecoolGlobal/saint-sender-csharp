c593fe9cf0c90b24d7450bb9c55f3884b77fce47
daniel-kalocsay <kdani782@gmail.com>
2019-12-03 11:19:21 +0100

    moved EmailService methods to partial classes

---
+++ SaintSender.Core/Services/EmailService.BuildEmail.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Apis.Gmail.v1.Data;
+using Org.BouncyCastle.Utilities.Encoders;
+using SaintSender.Core.Entities;
+
+namespace SaintSender.Core.Services
+{
+    partial class EmailService
+    {
+        private Email BuildEmail(Message message, string messageId)
+        {
+            IList<MessagePartHeader> messageHeaders = message.Payload.Headers;
+
+            string from = "";
+            string to = "";
+            string subject = "";
+            string date = "";
+
+            GetHeaders(ref from, ref to, ref subject, ref date, messageHeaders);
+
+            IList<MessagePart> messageParts = message.Payload.Parts;
+
+            StringBuilder bodyBuilder = new StringBuilder(); // lul
+            GetBodyFromNestedParts(messageId, messageParts, bodyBuilder);
+
+            if (bodyBuilder.Length == 0)
+            {
+                bodyBuilder.Append(message.Payload.Body.Data);
+            }
+
+            string plainBody = DecodeMessageBody(bodyBuilder);
+
+            return new Email(from, to, date, subject, plainBody);
+        }
+
+        private void GetHeaders(ref string from, ref string to, ref string subject, ref string date, IList<MessagePartHeader> messageHeaders)
+        {
+            foreach (MessagePartHeader header in messageHeaders)
+            {
+
+                if (header.Name == "From")
+                {
+                    from = header.Value;
+                }
+
+                else if (header.Name == "To")
+                {
+                    to = header.Value;
+                }
+
+                else if (header.Name == "Subject")
+                {
+                    subject = header.Value;
+                }
+
+                else if (header.Name == "Date")
+                {
+                    date = header.Value;
+                }
+            }
+        }
+
+        private void GetBodyFromNestedParts(string messageId, IList<MessagePart> messageParts, StringBuilder stringBuilder)
+        {
+            if (messageParts != null)
+            {
+                foreach (MessagePart messagePart in messageParts)
+                {
+
+                    SaveAttachments(messagePart, messageId);
+
+                    if (messagePart.MimeType == "text/plain")
+                    {
+                        stringBuilder.Append(messagePart.Body.Data);
+                    }
+
+                    if (messagePart.Parts != null)
+                    {
+                        GetBodyFromNestedParts(messageId, messagePart.Parts, stringBuilder);
+                    }
+                }
+
+            }
+        }
+
+        private string DecodeMessageBody(StringBuilder bodyBuilder)
+        {
+            string sbContent = bodyBuilder.ToString();
+            byte[] bodyBytes;
+
+            try
+            {
+                bodyBytes = Base64.Decode(sbContent);
+            }
+            catch (System.FormatException)
+            {
+                sbContent = bodyBuilder.ToString().Replace("-", "+").Replace("_", "/");
+                bodyBytes = Base64.Decode(sbContent);
+            }
+
+            return Encoding.UTF8.GetString(bodyBytes);
+        }
+    }
+}
--- SaintSender.Core/Services/EmailService.cs
+++ SaintSender.Core/Services/EmailService.cs
@@ -1,32 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using GmailQuickstart;
 using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
-using Org.BouncyCastle.Utilities.Encoders;
 using SaintSender.Core.Entities;
 
 namespace SaintSender.Core.Services
 {
-    public class EmailService
+    public partial class EmailService
     {
-        private GmailService gmail;
+        private readonly GmailService _gmail;
 
         public EmailService()
         {
-            gmail = Gmail.GetService();
+            _gmail = Gmail.GetService();
         }
 
         public List<Email> GetEmails()
         {
             List<Email> emails = new List<Email>();
 
-            UsersResource.MessagesResource.ListRequest messageRequest = gmail.Users.Messages.List("me");
+            UsersResource.MessagesResource.ListRequest messageRequest = _gmail.Users.Messages.List("me");
 
             messageRequest.MaxResults = 20;
             messageRequest.IncludeSpamTrash = true;
@@ -36,7 +30,7 @@ namespace SaintSender.Core.Services
 
             foreach (Message message in messageResponse.Messages)
             {
-                Message messageInfo = gmail.Users.Messages.Get("me", message.Id).Execute();
+                Message messageInfo = _gmail.Users.Messages.Get("me", message.Id).Execute();
 
                 Email email = BuildEmail(messageInfo, message.Id);
                 emails.Add(email);
@@ -45,143 +39,5 @@ namespace SaintSender.Core.Services
             return emails;
         }
 
-        private Email BuildEmail(Message message, string messageId)
-        {
-            IList<MessagePartHeader> messageHeaders = message.Payload.Headers;
-
-            string from = "";
-            string to = "";
-            string subject = "";
-            string date = "";
-
-            //GetHeaders(out from, out to, out subject, out date, messageHeaders);
-
-            foreach (MessagePartHeader header in messageHeaders)
-            {
-                //16
-                if (header.Name == "From")
-                {
-                    from = header.Value;
-                }
-                //20
-                else if (header.Name == "To")
-                {
-                    to = header.Value;
-                }
-                //19
-                else if (header.Name == "Subject")
-                {
-                    subject = header.Value;
-                }
-                //17
-                else if (header.Name == "Date")
-                {
-                    date = header.Value;
-                }
-            }
-
-
-            IList<MessagePart> messageParts = message.Payload.Parts;
-
-            StringBuilder bodyBuilder = new StringBuilder(); // lul
-            GetBodyFromNestedParts(messageId, messageParts, bodyBuilder); // message id here
-
-            if (bodyBuilder.Length == 0)
-            {
-                bodyBuilder.Append(message.Payload.Body.Data);
-            }
-
-            string sbContent = bodyBuilder.ToString();
-
-            byte[] bodyBytes;
-
-            try
-            {
-                bodyBytes = Base64.Decode(sbContent);
-            }
-            catch (System.FormatException)
-            {
-                sbContent = bodyBuilder.ToString().Replace("-", "+").Replace("_", "/");
-                bodyBytes = Base64.Decode(sbContent);
-            }
-
-            string plainBody = Encoding.UTF8.GetString(bodyBytes);
-
-            return new Email(from, to, date, subject, plainBody);
-
-        }
-
-        //private void GetHeaders(out string from, out string to, out string subject, out string date, IList<MessagePartHeader> messageHeaders)
-        //{
-        //    foreach (MessagePartHeader header in messageHeaders)
-        //    {
-
-        //        if (header.Name == "From")
-        //        {
-        //            from = header.Value;
-        //        }
-
-        //        else if (header.Name == "To")
-        //        {
-        //            to = header.Value;
-        //        }
-
-        //        else if (header.Name == "Subject")
-        //        {
-        //            subject = header.Value;
-        //        }
-
-        //        else if (header.Name == "Date")
-        //        {
-        //            date = header.Value;
-        //        }
-        //    }
-        //}
-
-        private void GetBodyFromNestedParts(string messageId, IList<MessagePart> messageParts, StringBuilder stringBuilder)
-        {
-
-            if (messageParts != null)
-            {
-                foreach (MessagePart messagePart in messageParts)
-                {
-                    if (!string.IsNullOrEmpty(messagePart.Filename))
-                    {
-                        string attId = messagePart.Body.AttachmentId;
-                        MessagePartBody attachPart = gmail.Users.Messages.Attachments.Get("me", messageId, attId).Execute();
-
-                        // Converting from RFC 4648 base64 to base64url encoding
-                        // see http://en.wikipedia.org/wiki/Base64#Implementations_and_history
-                        string attachData = attachPart.Data.Replace('-', '+');
-                        attachData = attachData.Replace('_', '/');
-
-                        byte[] data = Convert.FromBase64String(attachData);
-
-                        string targetPath = $@"C:\Users\Dániel Kalocsay\Desktop\emailbackup\{messageId}";
-
-                        if (!Directory.Exists(targetPath))
-                        {
-                            Directory.CreateDirectory(targetPath);
-                            File.WriteAllBytes(Path.Combine(targetPath, messagePart.Filename), data);
-                        }
-
-                        File.WriteAllBytes(Path.Combine(targetPath, messagePart.Filename), data);
-
-                    }
-
-                    if (messagePart.MimeType == "text/plain")
-                    {
-                        stringBuilder.Append(messagePart.Body.Data);
-                    }
-
-                    if (messagePart.Parts != null)
-                    {
-                        GetBodyFromNestedParts(messageId, messagePart.Parts, stringBuilder);
-                    }
-                }
-
-            }
-
-        }
     }
 }
