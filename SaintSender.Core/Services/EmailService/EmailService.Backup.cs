ee83af10f650f16027918f7b333cd3bbf7eaab9d
daniel-kalocsay <kdani782@gmail.com>
2019-12-05 11:35:30 +0100

    emailBackup refactored, still needs exception handling

--- SaintSender.Core/Services/EmailService/EmailService.Backup.cs
+++ SaintSender.Core/Services/EmailService/EmailService.Backup.cs
@@ -21,38 +21,16 @@ namespace SaintSender.Core.Services
             CreateFoldersForEmail(email, targetPath);
 
             string fileName = $@"{targetPath}\{email.Id}.txt";
-
-            FileStream fileStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
-
-            StreamWriter writer = new StreamWriter(fileStream);
-
-            writer.Write(email.Body);
-            writer.Close();
-
-
-            //File.WriteAllText(fileName, email.Body);
+            SaveEmailText(fileName, email.Body);
 
             string attachmentsPath = $@"{targetPath}\attachments";
 
-            if (!Directory.Exists(attachmentsPath))
+            if (email.Attachments.Count > 0)
             {
-                Directory.CreateDirectory(attachmentsPath);
+                CreateFolderForAttachments(attachmentsPath);
+                SaveAttachments(attachmentsPath, email.Attachments);
             }
 
-            int attCount = 0;
-            foreach (byte[] attachment in email.Attachments)
-            {
-                try
-                {
-                    string path = $@"{attachmentsPath}\{attCount}";
-                    File.WriteAllBytes(path, attachment);
-                    attCount++;
-                }
-                catch (Exception)
-                {
-                    //stuff
-                }
-            }
         }
         private static string GenerateValidNames(Email email)
         {
@@ -80,6 +58,41 @@ namespace SaintSender.Core.Services
             if (!File.Exists(fileName))
             {
                 File.Create(fileName);
+            }
+        }
+
+        private static void SaveEmailText(string fileName, string emailBody)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read);
+            StreamWriter writer = new StreamWriter(fileStream);
+
+            writer.Write(emailBody);
+            writer.Close();
+        }
+
+        private static void CreateFolderForAttachments(string attachmentsPath)
+        {
+            if (!Directory.Exists(attachmentsPath))
+            {
+                Directory.CreateDirectory(attachmentsPath);
+            }
+        }
+
+        private static void SaveAttachments(string attachmentsPath, List<byte[]> attachments)
+        {
+            int attCount = 0;
+            foreach (byte[] attachment in attachments)
+            {
+                try
+                {
+                    string path = $@"{attachmentsPath}\{attCount}";
+                    File.WriteAllBytes(path, attachment);
+                    attCount++;
+                }
+                catch (Exception)
+                {
+                    //stuff
+                }
             }
         }
     }
