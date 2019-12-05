4fb7a0a21c5495cc80106066a669af920a71cb85
daniel-kalocsay <kdani782@gmail.com>
2019-12-05 14:52:50 +0100

    close email window on saving, updated gitignore

--- SaintSender.DesktopUI/Views/EmailWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/EmailWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,6 +32,8 @@ namespace SaintSender.DesktopUI.Views
         {
             Email currentEmail = (Email)this.DataContext;
             EmailService.BackupEmail(currentEmail);
+            Thread.Sleep(500);
+            this.Close();
         }
     }
 }
